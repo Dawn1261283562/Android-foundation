764279db15268fad52a29e2d881694fdddcaf71b
“Dawn1261283562” <1261283562@qq.com>
2021-09-21 23:37:20 +0800

    Signed-off-by: “Dawn1261283562” <“1261283562@qq.com”>

--- client/app/src/main/java/com/example/asus/httpcallbacklistener/HttpCallbackListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.asus.httpcallbacklistener;
-
-//请求回调接口
-public interface HttpCallbackListener {
-
-    void onFinish(String response);
-
-    void onError(Exception e);
-}
--- codeOfDemo1/springJava/src/main/java/com/example/demo1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo1;
-
-public class HelloWorld {
-    public void say() {
-        System.out.println("hello,欢迎和【路人甲Java】一起学spring!");
-    }
-}
