db5d0a50dc2dfc998e4205ff470b59eae0489cdd
“Dawn1261283562” <1261283562@qq.com>
2021-10-23 18:14:50 +0800

    Signed-off-by: “Dawn1261283562” <“1261283562@qq.com”>

--- codeOfDemo1/springJava/src/main/java/com/example/demo1/service/StockService.java
+++ codeOfDemo1/springJava/src/main/java/com/example/demo1/service/StockService.java
@@ -3,7 +3,15 @@ package com.example.demo1.service;
 import com.example.demo1.entity.Stock;
 import com.example.demo1.entity.User;
 
+import java.util.List;
+
 public interface StockService {
 
     int update(Stock stock);
+
+    List<Stock> getById(Stock stock);
+
+    List<Stock> getByOther(String id);
+
+    String getFormalId(String id);
 }
