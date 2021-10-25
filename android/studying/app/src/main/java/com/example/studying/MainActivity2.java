4a82b73c121a6de4d63a29ae2e9baea406cbb44d
“Dawn1261283562” <1261283562@qq.com>
2021-10-25 22:16:30 +0800

    Signed-off-by: “Dawn1261283562” <“1261283562@qq.com”>

--- android/studying/app/src/main/java/com/example/studying/MainActivity2.java
+++ android/studying/app/src/main/java/com/example/studying/MainActivity2.java
@@ -61,6 +61,7 @@ public class MainActivity2 extends FragmentActivity implements View.OnClickListe
     private TextView titleTex;
 
 
+    private ArrayList<FundHeavyInfo> fundInfoList=new ArrayList<FundHeavyInfo>();
 
 
     SearchFragment1 searchFragment1;
@@ -71,6 +72,7 @@ public class MainActivity2 extends FragmentActivity implements View.OnClickListe
 
     private    ArrayList<FundHeavyInfo> temp;
     private ArrayList<Stock> stockList=new ArrayList<Stock>();
+    private ArrayList<String> typeList=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -160,15 +162,23 @@ public class MainActivity2 extends FragmentActivity implements View.OnClickListe
         int i=getIntent().getIntExtra("i",0);
         Intent intent = this.getIntent();
         stockList = (ArrayList<Stock>) intent.getSerializableExtra("stockList");
+        typeList = (ArrayList<String>) intent.getSerializableExtra("typeList");
         initData();
         mViewPager.setCurrentItem(i);
+
 
         resetTab();
         selectTab(i);
 
+
         if(stockList!=null){
 
             selectTab(2);
+        }
+        else if(typeList!=null){
+
+            System.out.println(typeList.get(0));
+            selectTab(3);
         }
         else return;
         //System.out.println(stockList.get(1).getName());
@@ -178,7 +188,7 @@ public class MainActivity2 extends FragmentActivity implements View.OnClickListe
         searchFragment1=new SearchFragment1();
         searchFragment2=new SearchFragment2();
         searchFragment3_1=new SearchFragment3_1(stockList);
-        searchFragment3_2=new SearchFragment3_2();
+        searchFragment3_2=new SearchFragment3_2(typeList);
         searchFragment3_10=new SearchFragment3_10();
 
         mFragments = new ArrayList<>();
@@ -372,6 +382,7 @@ public class MainActivity2 extends FragmentActivity implements View.OnClickListe
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
                         ResponseBody data = response.body();
+                        //if(response.body().string()==null)return;
                         String strByJson = response.body().string();
 
                         JsonParser parser = new JsonParser();
@@ -389,7 +400,12 @@ public class MainActivity2 extends FragmentActivity implements View.OnClickListe
                             //使用GSON，直接转成Bean对象
                             FundHeavyInfo fundHeavyInfoBean = gson.fromJson(fundHeavyInfo, FundHeavyInfo.class);
                             fundHeavyInfoList.add(fundHeavyInfoBean);
-
+                            if(fundHeavyInfoBean==null){
+                                Looper.prepare();
+                                //System.out.println(data);
+                                Toast.makeText(MainActivity2.this, "暂无相关信息", Toast.LENGTH_SHORT).show();
+                                Looper.loop();
+                            }
                             System.out.println("这下面是 基金信息的代码、名字、全名、法人公司名、管理者");
                             System.out.println(fundHeavyInfoBean.getId());
                             System.out.println(fundHeavyInfoBean.getName());
@@ -408,6 +424,8 @@ public class MainActivity2 extends FragmentActivity implements View.OnClickListe
                 fundHeavyInfoBean.setFull_nameame("eee");
                 fundHeavyInfoList.add(fundHeavyInfoBean);*/
                         temp=fundHeavyInfoList;
+                        fundInfoList =fundHeavyInfoList;
+                        //System.out.println(stockList1);
 
 
 //                Toast.makeText(MainActivity2.this, fundHeavyInfoList.get(0).getName(), Toast.LENGTH_SHORT).show();
@@ -419,6 +437,7 @@ public class MainActivity2 extends FragmentActivity implements View.OnClickListe
                     }
                 });
                 searchFragment1.fundSearchResult();
+                searchFragment1.update(fundInfoList);
             }
         });
     }
--- android/studying/app/src/main/java/com/example/studying/SearchFragment1.java
+++ android/studying/app/src/main/java/com/example/studying/SearchFragment1.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 
 import com.example.studying.entity.FundHeavyInfo;
+import com.example.studying.entity.Stock;
 import com.example.studying.utils.HttpGetRequest;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -35,12 +36,45 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.example.studying.entity.Stock;
+import com.example.studying.utils.HttpGetRequest;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 public class SearchFragment1 extends androidx.fragment.app.Fragment {
     private View mView;
 
-    ListView listView;
-//    FundAdapter fundAdapter;
-    private List<FundGeneral> fundGeneralList=new ArrayList<>();
+    private ListView listView1;
+    private FundAdapter fundAdapter1;
+
+    private List<FundGeneral> fundGeneralList1=new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -55,23 +89,25 @@ public class SearchFragment1 extends androidx.fragment.app.Fragment {
         }*/
 
 
+        fundAdapter1=new FundAdapter(getContext(),R.layout.fund_item,fundGeneralList1);
         //获取持仓搜索结果
-        /*fundSearchResult();
+        //fundSearchResult();
 
-        System.out.println("okkkkkkkkkkkkkkkkkkk3333");
-        fundAdapter=new FundAdapter(getContext(),R.layout.fund_item,fundGeneralList);*/
+        //System.out.println("okkkkkkkkkkkkkkkkkkk3333");
+        listView1 = (ListView) mView.findViewById(R.id.list_search1);
+        listView1.setAdapter(fundAdapter1);
         //fundAdapter.notifyDataSetChanged();
 
-        listView = mView.findViewById(R.id.list_search1);
+        //listView = mView.findViewById(R.id.list_search1);
         //listView.setAdapter(fundAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 /*Intent intent=new Intent(getActivity(),);
                 intent.putExtra("item1",i);
                 startActivity(intent);*/
 
-                FundGeneral fundGeneral = fundGeneralList.get(i);
+                FundGeneral fundGeneral = fundGeneralList1.get(i);
                 Toast.makeText(getContext(), fundGeneral.getFund2().toString(), Toast.LENGTH_SHORT).show();
             }
         });
@@ -80,16 +116,16 @@ public class SearchFragment1 extends androidx.fragment.app.Fragment {
         return mView;
     }
     public void fundSearchResult() {
-        fundGeneralList.clear();
+        fundGeneralList1.clear();
         MainActivity2 activity2=(MainActivity2)getActivity();
         ArrayList<FundHeavyInfo> temp=activity2.getTemp();
         if(temp!=null){
             for(int i = 0;i < temp.size(); i ++){
                 FundGeneral fundGeneral=new FundGeneral(temp.get(i).getId(),temp.get(i).getName(),temp.get(i).getManager());
                 System.out.println(temp.get(i).getId()+" "+temp.get(i).getName()+" "+temp.get(i).getManager());
-                fundGeneralList.add(fundGeneral);
+                fundGeneralList1.add(fundGeneral);
             }
-            listView.setAdapter(new  FundAdapter(getActivity(),R.layout.fund_item,fundGeneralList));
+            listView1.setAdapter(new  FundAdapter(getActivity(),R.layout.fund_item,fundGeneralList1));
         }
         System.out.println("okkkkkkkkkkkkkkkkkkk2222");
 
@@ -97,6 +133,38 @@ public class SearchFragment1 extends androidx.fragment.app.Fragment {
         fundGeneralList.add(fundGeneral1);
         FundGeneral fundGeneral2=new FundGeneral("000001.SZ","平安银行","管理者");
         fundGeneralList.add(fundGeneral2);*/
+    }
+
+    public void update(ArrayList<FundHeavyInfo> fundInfoList) {
+
+        System.out.println(123321);
+        fundGeneralList1.clear();
+
+        int size = fundInfoList.size();
+        for (int i = 0; i < size; i++) {
+            FundHeavyInfo value = fundInfoList.get(i);
+            FundGeneral fundGeneral1=new FundGeneral((String)value.getId(),(String) value.getName(),(String) value.getManager());
+            fundGeneral1.setFundHeavyInfo(value);
+            fundGeneralList1.add(fundGeneral1);
+
+
+        }
+
+        listView1.setAdapter(new FundAdapter(getActivity(),R.layout.fund_item,fundGeneralList1));
+
+        //Toast.makeText(getActivity(), "gengaile", Toast.LENGTH_SHORT).show();
+//        FundAdapter fundAdapter=new FundAdapter(getContext(),R.layout.fund_item,fundGeneralList);
+//
+//        listView = (ListView) mView.findViewById(R.id.list_search2);
+//        listView.setAdapter(fundAdapter);
+
+        //BB.performClick();
+        System.out.println(fundGeneralList1.size());
+        fundAdapter1.notifyDataSetChanged();
+//        FundGeneral fundGeneral1=new FundGeneral("000001.SZ","平安银行","20.04");
+//        fundGeneralList.add(fundGeneral1);
+
+
     }
 
     @Override
