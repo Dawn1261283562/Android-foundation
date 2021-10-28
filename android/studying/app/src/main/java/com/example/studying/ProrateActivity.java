91285b423488d236873c88e1adbe2cf0b71e2a3f
an-dou <759055535@qq.com>
2021-10-28 10:37:41 +0800

    Signed-off-by: an-dou <759055535@qq.com>

---
+++ android/studying/app/src/main/java/com/example/studying/ProrateActivity.java
@@ -0,0 +1,14 @@
+package com.example.studying;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProrateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prorate);
+    }
+}
