0798392de6f4e0d68949c431788340b350d36011
201930390166 <2829740863@qq.com>
2021-11-23 04:40:20 +0800

    修改了stockinfo页样式

--- android/studying/app/src/main/java/com/example/studying/Stockinfo.java
+++ android/studying/app/src/main/java/com/example/studying/Stockinfo.java
@@ -1,10 +1,13 @@
 package com.example.studying;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,11 +18,14 @@ import android.graphics.BitmapFactory;
 import android.os.StrictMode;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.studying.entity.Stock;
@@ -43,9 +49,13 @@ public      class Stockinfo extends AppCompatActivity{
     private TextView textView2;
     private TextView textView3;
     private ImageView imageview;
+    private ListView listView_bargin;
+
+    private String barginString;
     private Stock stockGet;
     private String type="asdasdasd";
     private TabLayout mTabTl;
+    private int barginnum=11;
 
 
     @Override
@@ -67,10 +77,11 @@ public      class Stockinfo extends AppCompatActivity{
         taa=(TextView)this.findViewById(R.id.aa);
         mTabTl=(TabLayout)this.findViewById(R.id.Stockinfo_tab) ;
 
-        textView2=(TextView) this.findViewById(R.id.text2);
-        textView3=(TextView) this.findViewById(R.id.text3);
+//        textView2=(TextView) this.findViewById(R.id.text2);
+//        textView3=(TextView) this.findViewById(R.id.text3);
         textView_code=(TextView) this.findViewById(R.id.code_style);
 
+        listView_bargin=(ListView)this.findViewById(R.id.View_bargin);
 
         StrictMode.setThreadPolicy(new
                 StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
@@ -86,6 +97,7 @@ public      class Stockinfo extends AppCompatActivity{
         String temp1=stockId.substring(0,6);
         String temp2=stockId.substring(7,9);
 
+
         temp2=temp2.toLowerCase(Locale.ROOT);
 //        //System.out.println(temp2+temp1);
         String stockIdFormal=temp2+temp1;
@@ -98,7 +110,6 @@ public      class Stockinfo extends AppCompatActivity{
         Log.v("stock_",url);
         initRequest(url);
         textView_code.setText(temp1);
-
 
         initimage(stockIdFormal );
 
@@ -185,9 +196,105 @@ public      class Stockinfo extends AppCompatActivity{
 
         });
 
+
     }
 
 
+    class bargin extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return barginnum+1;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            if(position==0)
+            {
+                return "此为属性名称";
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            if(position==0){
+                return false;
+            }
+            return super.isEnabled(position);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            View v=View.inflate(Stockinfo.this,R.layout.stock_bagin_view,null );
+            TextView t1=v.findViewById(R.id.text1);
+            TextView t2=v.findViewById(R.id.text2);
+            TextView t3=v.findViewById(R.id.text3);
+
+            t2.setTextColor(Color.parseColor("#c92027"));
+            List<String>bargin_list=Arrays.asList(getBarginString().split(","));
+            Log.v("bargin_",bargin_list.get(29));
+
+
+            if(position!=0){
+                if(position<=1){
+
+                    t1.setText("类别");
+                    t2.setText("金额/元");
+                    t3.setText("股数/手");
+                    t1.setTextColor(getColor(R.color.black));
+                    t2.setTextColor(getColor(R.color.black));
+                    t3.setTextColor(getColor(R.color.black));
+                    t1.setTextSize(16);
+                    t2.setTextSize(16);
+                    t3.setTextSize(16);
+
+
+                    return v;
+                }
+                else if(position<=6){
+
+                    t1.setText(position_to_barginstyle(position));
+                    t2.setText(bargin_list.get(33-2*position));
+                    t3.setText(wrapnum(bargin_list.get(32-2*position)));
+                    return v;
+                }
+                else{
+
+                    t1.setText(position_to_barginstyle(position));
+                    t2.setText(bargin_list.get(2*position-3));
+                    t3.setText(wrapnum(bargin_list.get(2*position-4)));
+
+                    return v;
+                }
+
+            }
+            else
+            {
+
+
+
+
+                t1.setTextColor(getColor(R.color.black));
+                t1.setTextSize(16);
+                t1.setText(position_to_barginstyle(position));
+                t2.setText(wrapamount(bargin_list.get(9))+"万元");
+                t3.setText(wrapnum(bargin_list.get(8))+"手");
+
+                return  v;
+            }
+        }
+    }
     public Bitmap getBitmap(String path) throws IOException {
         try {
             URL url = new URL(path);
@@ -225,6 +332,8 @@ public      class Stockinfo extends AppCompatActivity{
                 String res2=res.replace(" ", "");
 
 
+                setBarginString(res2);
+                Log.v("bargin_",getBarginString());
                 List<String> list= Arrays.asList(res2.split(","));
                 final String res3=list.get(0);
 
@@ -236,6 +345,8 @@ public      class Stockinfo extends AppCompatActivity{
 
 
 
+                        Stockinfo.bargin adapter_bargin=new Stockinfo.bargin();
+                        listView_bargin.setAdapter(adapter_bargin);
 
 
 
@@ -258,7 +369,6 @@ public      class Stockinfo extends AppCompatActivity{
                         }
                         taa.setText("当前价格:");
                         textView.setText( list2.get(1));
-
                         text_price_now.setText(list.get(3));
 
 
@@ -274,24 +384,24 @@ public      class Stockinfo extends AppCompatActivity{
                         text_price_l.setText("最低价:"+list.get(5));
 
 
-                        text_price_total.setText("总额:"+num2+"万");
-                        text_price_num.setText("总手:"+num1+"百股");
-                        textView2.setText(Html.fromHtml("<font>"
-                                +"今日成交：" +num1+"百股"+"<br>"
-                                +"买一：" +list.get(11)+"股数："+list.get(10) +"股"+"<br>"
-                                +"买二：" +list.get(13)+"股数："+list.get(12) +"股"+"<br>"
-                                +"买三：" +list.get(15)+"股数："+list.get(14) +"股"+"<br>"
-                                +"买四：" +list.get(17)+"股数："+list.get(16) +"股"+"<br>"
-                                +"买五：" +list.get(19)+"股数："+list.get(18) +"股"
-                                + "</font>"));
-                        textView3.setText(Html.fromHtml("<font>"
-                                +"总额："+num2+"万元"+"<br>"
-                                +" 卖一：" +list.get(21)+"股数："+list.get(20)+"<br>"
-                                +" 卖二：" +list.get(23)+"股数："+list.get(22)+"<br>"
-                                +" 卖三：" +list.get(25)+"股数："+list.get(24)+"<br>"
-                                +" 卖四：" +list.get(27)+"股数："+list.get(26)+"<br>"
-                                +" 卖五：" +list.get(29)+"股数："+list.get(28)
-                                + "</font>"));
+//                        text_price_total.setText("总额:"+num2+"万");
+//                        text_price_num.setText("总手:"+num1+"百股");
+//                        textView2.setText(Html.fromHtml("<font>"
+//                                +"今日成交：" +num1+"百股"+"<br>"
+//                                +"买一：" +list.get(11)+"股数："+list.get(10) +"股"+"<br>"
+//                                +"买二：" +list.get(13)+"股数："+list.get(12) +"股"+"<br>"
+//                                +"买三：" +list.get(15)+"股数："+list.get(14) +"股"+"<br>"
+//                                +"买四：" +list.get(17)+"股数："+list.get(16) +"股"+"<br>"
+//                                +"买五：" +list.get(19)+"股数："+list.get(18) +"股"
+//                                + "</font>"));
+//                        textView3.setText(Html.fromHtml("<font>"
+//                                +"总额："+num2+"万元"+"<br>"
+//                                +" 卖一：" +list.get(21)+"股数："+list.get(20)+"<br>"
+//                                +" 卖二：" +list.get(23)+"股数："+list.get(22)+"<br>"
+//                                +" 卖三：" +list.get(25)+"股数："+list.get(24)+"<br>"
+//                                +" 卖四：" +list.get(27)+"股数："+list.get(26)+"<br>"
+//                                +" 卖五：" +list.get(29)+"股数："+list.get(28)
+//                                + "</font>"));
 
                     }
                 });
@@ -310,5 +420,62 @@ public      class Stockinfo extends AppCompatActivity{
             e.printStackTrace();
         }
     }
+    public void setBarginString(String b){
+        this.barginString=b;
+    }
+    public String getBarginString(){
+        return this.barginString;
+    }
+
+    public String position_to_barginstyle(int p){
+        switch (p){
+            case 0:
+                return "总成交";
+            case 2:
+                return "卖五";
+            case 3:
+                return "卖四";
+            case 4:
+                return "卖三";
+            case 5:
+                return "卖二";
+            case 6:
+                return "卖一";
+            case 7:
+                return "买一";
+            case 8:
+                return "买二";
+            case 9:
+                return "买三";
+            case 10:
+                return "买四";
+            case 11:
+                return "买五";
+            default:
+                return "null";
+
+        }
+
+    }
+    public String wrapamount(String a){
+
+        final double temp2 =  Double.valueOf(a).doubleValue()/10000;
+
+        java.text.DecimalFormat myformat=new java.text.DecimalFormat("0.00");
+
+
+        String num2 =myformat.format(temp2);
+        return num2;}
+    public String wrapnum(String n){
+        final double temp1 =  Double.valueOf(n).doubleValue()/100;
+
+
+        java.text.DecimalFormat myformat=new java.text.DecimalFormat("0.00");
+
+        String num1 =myformat.format(temp1);
+        return num1;
+    };
+
 
 }
+
